5a0f4c1fc0468804fdad92499f17c25bfffb5a4f
Rik van Riel <riel@redhat.com>
2017-07-12 14:36:17 -0700

    random,stackprotect: introduce get_random_canary function
    Patch series "stackprotector: ascii armor the stack canary", v2.
    
    Zero out the first byte of the stack canary value on 64 bit systems, in
    order to mitigate unterminated C string overflows.
    
    The null byte both prevents C string functions from reading the canary,
    and from writing it if the canary value were guessed or obtained through
    some other means.
    
    Reducing the entropy by 8 bits is acceptable on 64-bit systems, which
    will still have 56 bits of entropy left, but not on 32 bit systems, so
    the "ascii armor" canary is only implemented on 64-bit systems.
    
    Inspired by the "ascii armor" code in execshield and Daniel Micay's
    linux-hardened tree.
    
    Also see https://github.com/thestinger/linux-hardened/
    
    This patch (of 5):
    
    Introduce get_random_canary(), which provides a random unsigned long
    canary value with the first byte zeroed out on 64 bit architectures, in
    order to mitigate non-terminated C string overflows.
    
    The null byte both prevents C string functions from reading the canary,
    and from writing it if the canary value were guessed or obtained through
    some other means.
    
    Reducing the entropy by 8 bits is acceptable on 64-bit systems, which
    will still have 56 bits of entropy left, but not on 32 bit systems, so
    the "ascii armor" canary is only implemented on 64-bit systems.
    
    Inspired by the "ascii armor" code in the old execshield patches, and
    Daniel Micay's linux-hardened tree.
    
    Change-Id: Ie16a7ac0e46aa4b97866bfbdfbaa3ae96ca401f2
    Link: http://lkml.kernel.org/r/20170524155751.424-2-riel@redhat.com
    Signed-off-by: Rik van Riel <riel@redhat.com>
    Acked-by: Kees Cook <keescook@chromium.org>
    Cc: Daniel Micay <danielmicay@gmail.com>
    Cc: "Theodore Ts'o" <tytso@mit.edu>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Git-commit: 022c204040f3fd22d6445bc35517786195b7ae80
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>

--- include/linux/random.h
+++ include/linux/random.h
@@ -50,6 +50,28 @@ bool rng_is_initialized(void);
 int wait_for_random_bytes(void);
 int register_random_ready_notifier(struct notifier_block *nb);
 int unregister_random_ready_notifier(struct notifier_block *nb);
+/*
+ * On 64-bit architectures, protect against non-terminated C string overflows
+ * by zeroing out the first byte of the canary; this leaves 56 bits of entropy.
+ */
+#ifdef CONFIG_64BIT
+# ifdef __LITTLE_ENDIAN
+#  define CANARY_MASK 0xffffffffffffff00UL
+# else /* big endian, 64 bits: */
+#  define CANARY_MASK 0x00ffffffffffffffUL
+# endif
+#else /* 32 bits: */
+# define CANARY_MASK 0xffffffffUL
+#endif
+
+static inline unsigned long get_random_canary(void)
+{
+	unsigned long val = get_random_long();
+
+	return val & CANARY_MASK;
+}
+
+unsigned long randomize_page(unsigned long start, unsigned long range);
 
 /* Calls wait_for_random_bytes() and then calls get_random_bytes(buf, nbytes).
  * Returns the result of the call to wait_for_random_bytes. */
